33e6a1e65110cf3593416f7b62b8389e321c836f
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-06-03 16:59:55 -0400

    in the middle of doing models

--- app_social/models.py
+++ app_social/models.py
@@ -10,4 +10,27 @@ class Profile(models.Model):
     return self.user.username
   
 
+class PostCategory(models.Model):
+  category = models.CharField(max_length=100, null=True)
 
+  def __str__(self):
+    return self.category
+
+class ReactionCategory(models.Model):
+  reaction = models.CharField(max_length=100, null=True)
+
+  def __str__(self):
+    return self.reaction
+
+class Image
+
+class Comment
+
+class Posts(models.Model):
+  created_on = models.DateTimeField(default=timezone.now)
+  post_category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, null=True)
+  post_author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+  reactions = models.ManyToManyField(ReactionCategory, on_delete=models.SET_NULL, null=True, default=blank)
+
+  def __str__(self):
+    return self.category

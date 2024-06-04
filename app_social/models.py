cd8c4e5fa6e3835717c12666002aa437177a13c8
Caitlin Schaeffer <caitlinschaef@gmail.com>
2024-06-04 10:01:17 -0400

    updated models

--- app_social/models.py
+++ app_social/models.py
@@ -23,14 +23,6 @@ class ReactionCategory(models.Model):
   def __str__(self):
     return self.reaction
 
-class Image(models.Model):
- title = models.TextField()
- created_at = models.DateTimeField(auto_now_add=True)
- image = models.ImageField(upload_to='images/')
-
- def __str__(self):
-   return self.title
-
 class Comment(models.Model):
   comment_author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
   text = models.CharField(max_length=500, null=True)
@@ -44,9 +36,10 @@ class Posts(models.Model):
   post_category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL, null=True)
   post_author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
   post_body = models.CharField(max_length=2000, null=True)
-  post_image = models.ForeignKey(Image, on_delete=models.SET_NULL, null=True, default=None)
+  image = models.ImageField(upload_to='images/', default=None)
+  image_caption = models.TextField(default=None)
   reactions = models.ManyToManyField(ReactionCategory, default=None)
   comments = models.ForeignKey(Comment, on_delete=models.SET_NULL, null=True)
 
   def __str__(self):
-    return f'Category: {self.post_category}, Author: {self.post_author}, {self.post_body} {self.post_image}, Posted: {self.created_on} {self.comments} {self.reactions}'
+    return f'Category: {self.post_category}, Author: {self.post_author}, {self.post_body} {self.image} {self.image_caption}, Posted: {self.created_on} {self.comments} {self.reactions}'
--- app_social/serializers.py
+++ app_social/serializers.py
@@ -11,10 +11,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     model = Profile
     fields = ['id', 'first_name', 'last_name']
 
-class ImageSerializer(serializers.ModelSerializer):
- class Meta:
-   models = Image
-   fields = ['id', 'title', 'image', 'created_at']
 
 class PostCategorySerializer(serializers.ModelSerializer):
   class Meta:
